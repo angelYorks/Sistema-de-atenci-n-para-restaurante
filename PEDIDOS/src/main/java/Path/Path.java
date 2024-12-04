7e738ca56e78c324f155db79cbc6dc99775757d1
Angel Y <yorkspalomino1816@gmail.com>
2024-12-04 18:39:26 -0500

    creacion

---
+++ PEDIDOS/src/main/java/Path/Path.java
@@ -0,0 +1,13 @@
+package Path;
+
+public class Path {
+
+    // windows
+    public static final String LOGIN = "/Login.fxml";
+    public static final String CLIENTEW = "/WinClient.fxml";
+    public static final String COCINAW = "/WinChef.fxml";
+    public static final String Alimento = "/Alimento.fxml";
+
+    // imagenes
+    public static final String LOGO = "/Imagenes/Logo_restaurant.png";
+}
